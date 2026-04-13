a7a36186250a5327921d8a9d3a6020dfd5d967ed
2005rishabh <161556896+2005rishabh@users.noreply.github.com>
2026-04-13 21:24:26 +0530

    Bulk sync: 13 submissions

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        String str = s.replaceAll("[^a-z0-9]", ""); // Keep letters AND digits
+        int n = str.length();
+
+        for (int i = 0; i < n / 2; i++) {
+            if (str.charAt(i) != str.charAt(n - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
